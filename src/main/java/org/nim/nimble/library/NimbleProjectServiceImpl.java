8ba9ed69bf0e872be08d5160ed2a897cbd5e9333
x99 <rapidtransit440@aol.com>
2019-12-28 08:27:48 -0500

    Reorganize some code (may break the parser) adding more comments.

--- src/main/java/org/nim/nimble/library/NimbleProjectServiceImpl.java
+++ src/main/java/org/nim/nimble/library/NimbleProjectServiceImpl.java
@@ -1,10 +1,16 @@
 package org.nim.nimble.library;
 
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@State(name = "NimbleProject", storages = @Storage(StoragePathMacros.WORKSPACE_FILE))
 public class NimbleProjectServiceImpl implements PersistentStateComponent<NimbleState> {
+
+
 
     @Nullable
     @Override
--- src/main/java/org/nim/sdk/NimSdk.java
+++ src/main/java/org/nim/sdk/NimSdk.java
@@ -1,35 +1,52 @@
 package org.nim.sdk;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkAdditionalData;
 import com.intellij.openapi.projectRoots.SdkModificator;
 import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.RootProvider;
+import com.intellij.openapi.roots.impl.libraries.LibraryImpl;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.UserDataHolderBase;
+import com.intellij.openapi.vfs.StandardFileSystems;
 import com.intellij.openapi.vfs.VirtualFile;
+import lombok.CustomLog;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@CustomLog
 public class NimSdk extends UserDataHolderBase implements Sdk, SdkModificator {
+
+    private NimSdk parent;
+    private NimSdkType type;
+    private String name;
+    private String version;
+    private String homePath;
+    private LibraryImpl rootProvider;
+
 
 
     @NotNull
     @Override
     public SdkTypeId getSdkType() {
-        return null;
+        return type;
     }
 
     @NotNull
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public void setName(@NotNull String name) {
-
+        this.name = name;
     }
 
     @Nullable
@@ -46,24 +63,27 @@ public class NimSdk extends UserDataHolderBase implements Sdk, SdkModificator {
     @Nullable
     @Override
     public String getHomePath() {
-        return null;
+        return homePath;
     }
 
     @Override
     public void setHomePath(String path) {
-
+        this.homePath = path;
     }
 
     @Nullable
     @Override
     public VirtualFile getHomeDirectory() {
-        return null;
+        if (homePath == null) {
+            return null;
+        }
+        return StandardFileSystems.local().findFileByPath(homePath);
     }
 
     @NotNull
     @Override
     public RootProvider getRootProvider() {
-        return null;
+        return rootProvider;
     }
 
     @NotNull
@@ -97,22 +117,28 @@ public class NimSdk extends UserDataHolderBase implements Sdk, SdkModificator {
 
     @Override
     public void addRoot(@NotNull VirtualFile root, @NotNull OrderRootType rootType) {
-
+        rootProvider.addRoot(root, rootType);
     }
 
     @Override
     public void removeRoot(@NotNull VirtualFile root, @NotNull OrderRootType rootType) {
-
+        rootProvider.removeRoot(root.getUrl(), rootType);
     }
 
     @Override
     public void removeRoots(@NotNull OrderRootType rootType) {
-
+        var urls = rootProvider.getUrls(rootType);
+        for(var url : urls){
+            rootProvider.removeRoot(url, rootType);
+        }
     }
 
     @Override
     public void removeAllRoots() {
-
+        var rootTypes = OrderRootType.getAllTypes();
+        for(var rootType : rootTypes){
+            removeRoots(rootType);
+        }
     }
 
     @Override
@@ -123,5 +149,13 @@ public class NimSdk extends UserDataHolderBase implements Sdk, SdkModificator {
     @Override
     public boolean isWritable() {
         return false;
+    }
+
+
+    public static LibraryImpl create(Project project, String name){
+        return (LibraryImpl) ApplicationManager.getApplication().runWriteAction((Computable<Library>) () -> {
+            var table = LibraryTablesRegistrar.getInstance().getLibraryTable(project);
+            return table.createLibrary(name);
+        });
     }
 }
